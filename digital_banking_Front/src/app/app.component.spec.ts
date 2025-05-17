3d176a4df5354eb732e13c15a61241ac0d6089e8
legend <karroumhoussam2020@gmail.com>
2025-05-17 16:33:30 +0100

    new frontend

--- digital_banking_Front/src/app/app.component.spec.ts
+++ digital_banking_Front/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'digital_banking_Front' title`, () => {
+  it(`should have as title 'digital_banking_Front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('digital_banking_Front');
--- digital_banking_Front/src/app/app.component.ts
+++ digital_banking_Front/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from './navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent],
   templateUrl: './app.component.html',
+  standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
